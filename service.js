6e706528357e3e054e967e1c4d122d9259376d16
Michael Sayer <michael.j.say@gmail.com>
2015-01-21 12:51:13 -0700

    not sure about this guy

--- service.js
+++ service.js
@@ -3,10 +3,27 @@ var app = angular.module('userProfiles');
 app.service('mainService', function($http) {
 
 
+var app = angular.module('userProfiles');
+
+// app.service('mainService', function($http) {
+//   this.getUsers = function() {
+//     return $http({
+//       method: 'GET',
+//       url: 'http://reqr.es/api/users?page=1'
+//     });
+//   }
+// });
+
+
   this.getUsers = function() {
     return $http({
         method: 'GET',
         url: 'http://reqr.es/api/users?page=3'
+    }).then(function(res) {
+    	return res;
     })
   }
+
+
+
 });
